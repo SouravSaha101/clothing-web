2b3f0de0af827d3e4342abd535e63a768b5cc308
Sourav <souravsaha101@gmail.com>
2021-04-15 16:32:28 +0530

    Project added

---
+++ src/homepage.component.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./homepage.styles.scss";
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">HAT</h1>
+            <span className="subtitle">SHOP NOw</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
