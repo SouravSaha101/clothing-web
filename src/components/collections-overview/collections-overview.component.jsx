653a3591f66bb7619c190f49aa76c1120c4af489
Sourav <souravsaha101@gmail.com>
2021-05-03 20:23:17 +0530

    route fixed

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,22 +1,23 @@
-import React from "react";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
-import "./collections-overview.styles.scss";
-import CollectionPreview from "../../components/collection-preview/collection-preview";
-import { selectCollections } from "../../redux/shop/shop.selector";
+import CollectionPreview from '../collection-preview/collection-preview.component';
 
-const CollectionsOverview = ({ collections }) => {
-  return (
-    <div className="collections-overview">
-      {collections.map(({ id, ...otherCollectionProps }) => (
-        <CollectionPreview key={id} {...otherCollectionProps} />
-      ))}
-    </div>
-  );
-};
-const mapStatetoProps = createStructuredSelector({
-  collections: selectCollections,
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
+
+import './collections-overview.styles.scss';
+
+const CollectionsOverview = ({ collections }) => (
+  <div className='collections-overview'>
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+);
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollectionsForPreview
 });
 
-export default connect(mapStatetoProps)(CollectionsOverview);
+export default connect(mapStateToProps)(CollectionsOverview);
---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51ImmRvSHC5a8u94V1pnFOhkAfD26TVbLIEjCrDAQ5g5i7GX9NXoK3BwrMCgiUfu2TRDVFT4W7zhsYFH3Sba9ViGk00ZZ8LVZSv";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Succesful!");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Clothing web."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
